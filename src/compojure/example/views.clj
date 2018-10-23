3d19f3ba4ae2cce134c7dcc1a8bb1d1d45d0263a
Dmitry Kotik <dkotik@yandex-team.ru>
2018-10-23 23:30:38 +0300

    main page

--- src/compojure/example/views.clj
+++ src/compojure/example/views.clj
@@ -1,17 +1,23 @@
 (ns compojure.example.views
   (:require [net.cgrand.enlive-html :as html]))
 
-(html/deftemplate page-index "./../resources/index.html" [ctxt]
-  [:title] (html/content "Awesome application")
-  [:#old] (html/content (:old ctxt))
-  [:#msg2] (html/set-attr "style" "display: none"))
 
+(html/deftemplate main-page-index "./../resources/main.html" [ctxt]
+  [:title] (html/content (:title ctxt))
+  [:#title] (html/content (:title ctxt))
+  [:#subtitle] (html/content (:subtitle ctxt))
+  [:#description] (html/content (:description ctxt))
+  [:body] (html/set-attr "style" "background-color: #000; color: #fff"))
 
 (html/deftemplate auth-index "./../resources/signin.html" [ctxt]
   [:title] (html/content "Sign in"))
   
 (defn index-page []
-  (page-index {:old "123"}))
+  (main-page-index {
+    :title "Natalia Valkova"
+    :subtitle "Designer"
+    :description "profile"
+  }))
 
 (defn auth-page []
   (auth-index {}))
